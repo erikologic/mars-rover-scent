efc653fa1763616b0cbb94a71540a934572ca739
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-03-29 12:33:24 +0000

    RED checking coordinates

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,11 @@
-it("works", () => expect(1).toBe(1));
+function main(input: string) {
+    throw new Error('Not implemented')
+}  
+
+it('fails on max value', () => {
+    const input = `500 3
+1 1 E
+R`
+
+    expect(() => main(input)).toThrow('Max coordinate value is 50')
+})
