751c69d62befc4946c9edf09016c9d25e2105bde
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-03-29 17:18:22 +0000

    🔵 extrapolating validating function

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,26 @@ export function main(input: string): void {
   validateBoardXandY(input)
 }
 
+function breaksLimit(n: number): undefined | ['min' | 'max', number] {
+  if (n < 0) return ['min', 0]
+  if (n > 50) return ['max', 50]
+}
+
 function validateBoardXandY(input: string): void {
   const inputBoard = input.split('\n')[0]
   const [x, y] = inputBoard.split(' ').map(Number)
+  const coordinates = { x, y }
 
-  if (x < 0) {
+  const key = 'x'
+  const axis = key.toUpperCase()
+  const value = coordinates[key]
+  const limitBroken = breaksLimit(value)
+  if (limitBroken !== undefined) {
+    const [limit, threshold] = limitBroken
     throw new Error(
-      `Error while parsing the board X coordinate: value is ${x} but the min value can only be 0`
+      `Error while parsing the board ${axis} coordinate: value is ${value} but the ${limit} value can only be ${threshold}`
     )
   }
-
-  if (x > 50)
-    throw new Error(
-      `Error while parsing the board X coordinate: value is ${x} but the max value can only be 50`
-    )
 
   if (y < 0) {
     throw new Error(
