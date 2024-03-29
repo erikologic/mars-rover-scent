8d453f5289710c6b515ffe64752c581ecebcd791
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-03-29 16:52:27 +0000

    🟢

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ export function main(input: string): void {
       'Error while parsing the board 1st coordinate: value is -1 but the min value can only be 0'
     )
   }
+
+  if (y < 0) {
+    throw new Error(
+      'Error while parsing the board 2nd coordinate: value is -1 but the min value can only be 0'
+    )
+  }
+
   if (y > 50)
     throw new Error(
       `Error while parsing the board 2nd coordinate: value is ${y} but the max value can only be 50`
