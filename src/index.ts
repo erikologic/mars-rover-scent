9da098bb74f6f7f69b481b8bf920f08e9720f08c
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-03-29 15:03:38 +0000

    🔵 refactor test

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,7 @@ R`
     throw new Error(
       'Error while parsing the board 2nd coordinate: value is 500 but the max value can only be 50'
     )
-  throw new Error('Max coordinate value is 50')
+  throw new Error(
+    'Error while parsing the board 1st coordinate: value is 99 but the max value can only be 50'
+  )
 }
