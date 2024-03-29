06d09bac52b7656e8c239085bddf663b6ebfe159
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-03-29 12:40:00 +0000

    🔴 2nd board coordinate

--- src/test.ts
+++ src/test.ts
@@ -7,3 +7,11 @@ R`
 
     expect(() => main(input)).toThrow('Max coordinate value is 50')
 })
+
+it('fails on max coordinate value for the 2nd board coordinate', () => {
+    const input = `5 500
+1 1 E
+R`
+
+    expect(() => main(input)).toThrow('Error while parsing the board 2nd coordinate: max coordinate value is 50')
+})
