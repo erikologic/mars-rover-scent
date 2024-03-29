c3269560079acd000cd2c7a2d9efff03e5147d13
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-03-29 16:41:55 +0000

    🔵 generalised

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 export function main(input: string): void {
   const inputBoard = input.split('\n')[0]
-  if (inputBoard === `5 900`)
+  const [_, y] = inputBoard.split(' ').map(Number)
+  if (y > 50)
     throw new Error(
-      'Error while parsing the board 2nd coordinate: value is 900 but the max value can only be 50'
+      `Error while parsing the board 2nd coordinate: value is ${y} but the max value can only be 50`
     )
 
-  if (inputBoard === `5 500`)
-    throw new Error(
-      'Error while parsing the board 2nd coordinate: value is 500 but the max value can only be 50'
-    )
   throw new Error(
     'Error while parsing the board 1st coordinate: value is 99 but the max value can only be 50'
   )
