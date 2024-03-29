00245284691cd033864b11b2199c5f83c8838496
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-03-29 16:49:43 +0000

    🟢

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 export function main(input: string): void {
   const inputBoard = input.split('\n')[0]
-  const [_, y] = inputBoard.split(' ').map(Number)
+  const [x, y] = inputBoard.split(' ').map(Number)
+
+  if (x < 0) {
+    throw new Error(
+      'Error while parsing the board 1st coordinate: value is -1 but the min value can only be 0'
+    )
+  }
   if (y > 50)
     throw new Error(
       `Error while parsing the board 2nd coordinate: value is ${y} but the max value can only be 50`
